7b71a0f6f6745f945004a38af59515e567e9266a
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-02-10 15:42:19 +0530

    first commit

---
+++ final.py
@@ -0,0 +1,481 @@
+from colorsys import ONE_SIXTH
+from os import link
+from re import X
+from statistics import mean
+import instaloader
+from instaloader import  Profile
+from datetime import datetime
+from itertools import dropwhile, takewhile
+import statistics 
+from os import listdir
+import os
+from os.path import isfile, join
+from re import L
+from PIL import Image
+import instaloader
+from instaloader import Profile 
+import time 
+import shutil
+import datetime
+from os import listdir
+import os
+from os.path import isfile, join
+from re import L
+
+
+percentage=0
+users = []
+
+# Create an instance of Instaloader class
+ig = instaloader.Instaloader()
+
+#req_user = input("Enter your username: ")
+username= "ravi_chawla_9"
+#password= input("Enter your password: ")
+ig.login("johnbhai_07","Piyush123")
+
+# Load a profile from an Instagram handle
+profile  = instaloader.Profile.from_username(ig.context, username)
+
+
+
+
+# user = profile .username
+# no_of_post =  profile .mediacount 
+# bio = profile .biography
+# links = profile .external_url
+# private = profile.is_private # displays True, if private account 
+#ig.download_profile(username , profile_pic_only=True)
+posts = profile.get_posts()
+
+no_of_followers = profile .followers 
+no_of_following =  profile .followees
+no_of_post =  profile .mediacount 
+ 
+
+followers_public_fake = []
+followers_public_real = []
+
+following_fake = []
+following_real = []
+
+ratios = []
+
+likes1=[1]
+for i in posts:
+    x = i.likes
+    if x == 0:
+        x = 1
+        likes1.append(x)
+    else: 
+        likes1.append(x)
+
+average_likes = statistics.mean(likes1)
+
+followers_following_ratio = no_of_followers / no_of_following
+post_followers_ratio = no_of_post / no_of_followers
+likes_followers_ratio = average_likes / no_of_followers
+
+
+ratios.append(followers_following_ratio)
+ratios.append(post_followers_ratio)
+ratios.append(likes_followers_ratio)
+
+#followers
+for followers in profile.get_followers():  
+
+    profile  = instaloader.Profile.from_username(ig.context, followers.username)
+    percentage = 0 
+    
+    user = profile .username
+    bio = profile .biography
+    links = profile .external_url
+    no_of_post =  profile .mediacount 
+    no_of_followers = profile.followers 
+    no_of_following =  profile.followees
+
+    
+    
+    #PROFILE COMPARISON 
+
+    ig.download_profile(user,profile_pic_only=True)
+     
+    file_names=[]
+
+    x = listdir(user)
+    for i in x:
+        file_names.append(i)
+    # #print(len(file_names))
+
+    unique_name=[]
+    for i in file_names:
+        if(i not in unique_name):
+            unique_name.append(i)
+    # #print(len(unique_name))
+
+    c=0
+
+    for i in unique_name:
+        # #print(i[-15:])    
+        if(i[-15:]=="profile_pic.jpg"):
+            os.rename("./{}/".format(user)+i,"./{}/profile_pic.jpg".format(user))
+        
+
+    img1 = Image.open("C:\\Users\\Dell\\Desktop\\Hackathon\\{}\\profile_pic.jpg".format(user)).convert('RGB')
+    img2 = Image.open("C:\\Users\\Dell\\Desktop\\Hackathon\\blankpof.jpg").convert('RGB')
+    if img1.im.bands != img2.im.bands:
+        #print('PROFILE IS NOT EMPTY')
+        exit(-1)
+
+    imdata1 = list(img1.getdata())
+    imdata2 = list(img2.getdata())
+
+
+    diff = 0
+    if img1.im.bands == 1:
+        diff = sum([abs(float(i1) - float(i2)) for i1, i2 in zip(imdata1, imdata2)])
+        
+    else:
+        diff = sum([abs(float(i1) - float(i2)) for i1, i2 in
+                    zip([i for p in imdata1 for i in p],
+                        [i for p in imdata2 for i in p])])
+        
+        
+    if diff > 0:
+        pass
+        #print('Profile is not empty')
+        
+    else:
+
+        if(profile.is_private is False):
+            percentage += 25
+        else:
+            percentage += 35  
+        
+        #print("Profile is empty")
+
+    
+
+    # RATIOS 
+
+
+    if( no_of_followers == 0):
+        no_of_followers=1
+    if( no_of_following == 0):
+        no_of_following=1
+    followers_following_ratio = no_of_followers / no_of_following
+
+    if(no_of_post == 0):
+        no_of_post=1
+    post_followers_ratio = no_of_post / no_of_followers
+    
+    
+
+
+    if profile.is_private is False:
+        percentage += 5 
+
+
+        #LIKES_FOLLOWERS RATIO 
+
+
+        likes1=[1]
+        for i in posts:
+            x = i.likes
+            if x == 0:
+                x = 1
+                likes1.append(x)
+            else: 
+                likes1.append(x)
+
+        average_likes = statistics.mean(likes1)
+
+        if ( average_likes <= 0):
+            average_likes=1
+        if( no_of_followers == 0):
+            no_of_followers=1
+        likes_followers_ratio = average_likes / no_of_followers
+
+
+
+
+        if likes_followers_ratio < 0.25 and likes_followers_ratio > 0.20:
+            percentage += 5 
+
+        elif likes_followers_ratio < 0.20 and likes_followers_ratio > 0.167:
+            percentage += 7
+
+        elif likes_followers_ratio < 0.167:
+            percentage += 10
+
+
+
+
+        #FOLLOWERS_FOLLOWING RATIO 
+
+
+        if followers_following_ratio < 0.5 and followers_following_ratio > 0.3:
+            percentage += 10
+        elif followers_following_ratio < 0.3 and followers_following_ratio > 0.25:
+            percentage += 12 
+        elif followers_following_ratio < 0.25:
+            percentage += 15 
+
+
+
+
+        #BIO LENGTH 
+
+
+        if len(bio) == 0 or len(bio) >= 125:
+            percentage +=10
+
+
+  
+
+        #POST_FOLLOWERS RATIO 
+
+        if post_followers_ratio > 2 and post_followers_ratio < 3:
+            percentage += 10 
+        elif post_followers_ratio > 3 and post_followers_ratio < 4:
+            percentage += 12
+        elif post_followers_ratio > 4:
+            percentage += 15
+
+        if (percentage >= 60):
+            print(user,"fake")
+            followers_public_fake.append(user)
+        else:
+            print(user,"real")
+            followers_public_real.append(user)
+
+        #CAPTIONS
+  
+
+    else:
+
+
+        #FOLLOWERS_FOLLOWING RATIO
+
+
+        if followers_following_ratio < 0.5 and followers_following_ratio > 0.3:
+            percentage += 20
+        elif followers_following_ratio < 0.3 and followers_following_ratio > 0.25:
+            percentage += 23
+        elif followers_following_ratio < 0.25:
+            percentage += 25 
+
+
+        
+        #BIO LENGTH
+
+
+        if len(bio) == 0 or len(bio) >= 125:
+            percentage +=20 
+
+
+
+    
+        #POST_FOLLOWERS RATIO 
+
+
+        if post_followers_ratio > 2 and post_followers_ratio < 3:
+            percentage += 15 
+        elif post_followers_ratio > 3 and post_followers_ratio < 4:
+            percentage += 17
+        elif post_followers_ratio > 4:
+            percentage += 20
+
+    
+        if (percentage >= 75 ):
+            print(user,"fake")
+            following_fake.append(user)
+        else:
+            print(user,"real")
+            following_real.append(user)
+
+    
+    shutil.rmtree('./{}'.format(user))
+
+#following
+
+
+
+for following in profile.get_followees(): 
+
+    profile  = instaloader.Profile.from_username(ig.context, following.username)
+    percentage = 0 
+    
+    user = profile .username
+    bio = profile .biography
+    links = profile .external_url
+    no_of_post =  profile .mediacount 
+    no_of_followers = profile.followers 
+    no_of_following =  profile.followees
+    if(no_of_post == 0):
+        no_of_post = 1
+    if(no_of_followers == 0):
+        no_of_followers = 1
+    if ( no_of_following == 0):
+        no_of_following = 1
+
+    
+    
+    #PROFILE COMPARISON 
+
+    ig.download_profile(user,profile_pic_only=True)
+
+    file_names=[]
+
+    x = listdir(user)
+    for i in x:
+        file_names.append(i)
+    # #print(len(file_names))
+
+    unique_name=[]
+    for i in file_names:
+        if(i not in unique_name):
+            unique_name.append(i)
+    # #print(len(unique_name))
+
+    c=0
+
+    for i in unique_name:
+        # #print(i[-15:])    
+        if(i[-15:]=="profile_pic.jpg"):
+            os.rename("./{}/".format(user)+i,"./{}/profile_pic.jpg".format(user))
+        
+
+    img1 = Image.open("C:\\Users\\Dell\\Desktop\\Hackathon\\{}\\profile_pic.jpg".format(user)).convert('RGB')
+    img2 = Image.open("C:\\Users\\Dell\\Desktop\\Hackathon\\blankpof.jpg").convert('RGB')
+    if img1.im.bands != img2.im.bands:
+        print('PROFILE IS NOT EMPTY')
+        exit(-1)
+
+    imdata1 = list(img1.getdata())
+    imdata2 = list(img2.getdata())
+
+
+    diff = 0
+    if img1.im.bands == 1:
+        diff = sum([abs(float(i1) - float(i2)) for i1, i2 in zip(imdata1, imdata2)])
+        
+    else:
+        diff = sum([abs(float(i1) - float(i2)) for i1, i2 in
+                    zip([i for p in imdata1 for i in p],
+                        [i for p in imdata2 for i in p])])
+        
+        
+    if diff > 0:
+        print('Profile is not empty')
+        
+    else:
+        print("Profile is empty")
+        percentage += 25
+
+
+    
+
+    # RATIOS 
+
+    if( no_of_followers == 0):
+        no_of_followers=1
+    if( no_of_following == 0):
+        no_of_following=1
+    followers_following_ratio = no_of_followers / no_of_following
+
+    if(no_of_post == 0):
+        no_of_post=1
+    post_followers_ratio = no_of_post / no_of_followers
+    
+    #PUBLIC OR PRIVATE
+
+    if profile.is_private is False:
+        percentage += 5
+    else:
+        pass
+
+    
+
+    # LIKES
+
+
+
+    likes1=[1]
+    for i in posts:
+        x = i.likes
+        if x == 0:
+            x = 1
+            likes1.append(x)
+        else: 
+            likes1.append(x)
+
+    average_likes = statistics.mean(likes1)
+
+    if ( average_likes <= 0):
+        average_likes=1
+    if( no_of_followers == 0):
+        no_of_followers=1
+    likes_followers_ratio = average_likes / no_of_followers
+
+
+
+    if likes_followers_ratio < 0.25 and likes_followers_ratio > 0.20:
+        percentage += 10 
+
+    elif likes_followers_ratio < 0.20 and likes_followers_ratio > 0.167:
+        percentage += 15
+
+    elif likes_followers_ratio < 0.167:
+        percentage += 20
+
+
+
+    #FOLLOWERS_FOLLOWING RATIO 
+
+
+
+    if followers_following_ratio < 0.5 and followers_following_ratio > 0.3:
+        percentage += 10
+    elif followers_following_ratio < 0.3 and followers_following_ratio > 0.25:
+        percentage += 15 
+    elif followers_following_ratio < 0.25:
+        percentage += 20 
+
+
+
+
+    #BIO LENGTH 
+
+
+    if len(bio) == 0 or len(bio) >= 125:
+        percentage +=5 
+
+
+
+    #POST_FOLLOWERS RATIO 
+
+    if post_followers_ratio > 2 and post_followers_ratio < 3:
+        percentage += 10 
+    elif post_followers_ratio > 3 and post_followers_ratio < 4:
+        percentage += 15
+    elif post_followers_ratio > 4:
+        percentage += 20
+
+    
+    
+
+    if (percentage >= 60 ):
+            print(user,"fake")
+            following_fake.append(user)
+    else:
+        print(user,"real")
+        following_real.append(user)
+
+    shutil.rmtree('./{}'.format(user))
+
+
+
+print(followers_public_fake,followers_public_real,following_fake,following_real,ratios)
+
+
---
+++ python logic/followers logic.py
@@ -0,0 +1,108 @@
+import instaloader
+from datetime import datetime
+from instagrapi import Client
+L = instaloader.Instaloader()
+username1 = input("Username: ")
+password1 = input("Password: ")
+profile = instaloader.Profile.from_username(L.context, username1)
+L.login(username1, password1)
+
+
+followers = profile.get_followers()
+followers_list = []
+for follower in followers:
+    followers_list.append(follower.username)
+
+
+cl = Client()
+cl.login(username1, password1)
+
+
+
+#====================================================================================================================
+#====================================================================================================================
+#====================================================================================================================
+
+
+
+real_followers = []
+fake_followers = []
+prob_real_followers = []
+prob_fake_followers = []
+percent = 0
+for j in followers_list:
+    percent = 0
+    dict=cl.user_info_by_username(j).dict()
+    if (dict["is_verified"] == True):
+        real_followers.append(j)
+    else:
+        try:
+            profile = instaloader.Profile.from_username(L.context, j)
+            posts = profile.get_posts()
+            post1 = next(posts)
+            post2 = next(posts)
+            timestamp1 = datetime.fromtimestamp(post1.date_local.timestamp())
+            timestamp2 = datetime.fromtimestamp(post2.date_local.timestamp())
+            duration = timestamp2 - timestamp1
+            if (abs(duration.days) == 1):
+                percent +=  0.2
+
+
+            likes1=[1]
+            for i in posts:
+                x = i.likes
+                if x == 0:
+                    x = 1
+                    likes1.append(x)
+                else: 
+                    likes1.append(x)
+            avg_likes = sum(likes1) / len(likes1)
+            likes_followers_ration = avg_likes / int(dict["follower_count"])
+            if (likes_followers_ration >= 0.33):
+                percent += 0.05
+            elif(likes_followers_ration >= 0.2):
+                percent += 0.1
+        except :
+            pass
+        
+        try:
+            if dict["is_private"] == False:
+                percent += 0.1
+            else:
+                percent += 0.05
+        except:
+            pass
+        try:
+            follower_following_ratio = int(dict['follower_count']) / int(dict['following_count'])
+            if (follower_following_ratio >= 0.5):
+                percent += 0.05
+            elif (follower_following_ratio < 0.5 and follower_following_ratio >= 0.33):
+                percent += 0.1
+            elif (follower_following_ratio < 0.33):
+                percent += 0.2
+        except:
+            pass
+            
+        try:
+            media_follower_ratio = int(dict["media_count"]) / int(dict["follower_count"])
+            if (media_follower_ratio >= 0.5):
+                percent += 0.05
+            elif (media_follower_ratio > 0.8):
+                percent += 0.1
+            elif (media_follower_ratio > 1):
+                percent += 0.2
+        except:
+            pass
+        try:
+            if (dict["biography"] == None) or (len(dict["biography"]) >= 125):
+                    percent += 0.2
+        except:
+            pass
+    if (percent >= 0.6):
+        fake_followers.append(j)
+    elif (percent >= 0.5 and percent < 0.6):
+        prob_fake_followers.append(j)
+    elif (percent < 0.5 and percent <= 0.35):
+        prob_real_followers.append(j)
+    elif (percent < 0.35):
+        real_followers.append(j)
